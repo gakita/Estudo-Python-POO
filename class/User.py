3cb738b1e82b2e9c0ab901363d863f18225b13e4
gakita <raulgru5@gmail.com>
2025-07-18 23:20:42 +0200

    docs: atualização de reqs

--- class/User.py
+++ class/User.py
@@ -1,7 +1,18 @@
 import utils
 
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
-class User(name, email, theme):
-    name = ""
-    email = ""
-    theme = utils
+    def get_user_info(self):
+        return {
+            "username": self.username,
+            "email": self.email
+        }
+
+    def update_email(self, new_email):
+        if utils.validate_email(new_email):
+            self.email = new_email
+            return True
+        return False
